a92a80901f868f4bd24c220da9892c2ef4b30a64
Kitamura <sdumais.dumais@gmail.com>
2026-06-08 00:18:39 -0400

    Still initial commit

--- Code/VirtualDrip/backends/vDrip9928/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/kitamura/Documents/Z80HomeBrew/Code/MODERN/vDrip9928
-# Build directory: /home/kitamura/Documents/Z80HomeBrew/Code/MODERN/vDrip9928
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
